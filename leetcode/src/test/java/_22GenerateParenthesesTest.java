2a2d8da9debba5077090fc9331db90baeaa2737e
Anton <anthony300787@gmail.com>
2019-05-23 23:18:37 +0800

    Leetcode-22

--- leetcode/src/test/java/_22GenerateParenthesesTest.java
+++ leetcode/src/test/java/_22GenerateParenthesesTest.java
@@ -1,6 +1,6 @@
 import org.junit.Test;
 
-import java.util.List;
+import java.util.*;
 
 import static org.junit.Assert.*;
 
@@ -10,9 +10,27 @@ import static org.junit.Assert.*;
 public class _22GenerateParenthesesTest {
 
     @Test
-    public void generateParenthesis() throws Exception {
-        List<String> result = new _22GenerateParentheses().generateParenthesis(3);
-        System.out.print(result);
+    public void generateParenthesis1() {
+        assertOutputEquals(new _22GenerateParentheses().generateParenthesis(1),
+                new ArrayList<>(Arrays.asList("()")));
+    }
+
+    @Test
+    public void generateParenthesis2() {
+        assertOutputEquals(new _22GenerateParentheses().generateParenthesis(2),
+                new ArrayList<>(Arrays.asList("()()", "(())")));
+    }
+
+    @Test
+    public void generateParenthesis3() {
+        assertOutputEquals(new _22GenerateParentheses().generateParenthesis(3),
+                new ArrayList<>(Arrays.asList("()()()", "()(())", "(())()", "((()))", "(()())")));
+    }
+
+    private void assertOutputEquals(List<String> expected, List<String> actual) {
+        Collections.sort(actual);
+        Collections.sort(expected);
+        assertEquals(expected, actual);
     }
 
 }
