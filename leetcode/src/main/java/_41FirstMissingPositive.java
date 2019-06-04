9ed2f3f39713eca2f77ffb5f0821358dae91591b
Anton <anthony300787@gmail.com>
2019-06-04 22:48:11 +0800

    Leetcode-416

--- leetcode/src/main/java/_41FirstMissingPositive.java
+++ leetcode/src/main/java/_41FirstMissingPositive.java
@@ -1,3 +1,6 @@
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by anton on 26/2/17.
  */
@@ -32,25 +35,21 @@ public class _41FirstMissingPositive {
 
     public int firstMissingPositive0(int[] nums) {
         int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-        int sum = 0;
-        int count = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > 0) {
-                min = Math.min(min, nums[i]);
-                max = Math.max(max, nums[i]);
-                sum += nums[i];
-                count++;
+        Set<Integer> positives = new HashSet<Integer>();
+        for (int n : nums) {
+            if (n > 0) {
+                min = Math.min(min, n);
+                positives.add(n);
             }
         }
+
         if (min > 1) {
             return 1;
-        }
-        int expectedSum = (int) ((1 + count) * 0.5 * (count));
-        if (sum == expectedSum) {
-            return count + 1;
         } else {
-            return expectedSum - (sum - max);
+            while (positives.contains(min)) {
+                min++;
+            }
+            return min;
         }
     }
 
