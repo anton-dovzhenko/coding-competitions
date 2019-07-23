c2852f2e1a0041afb1eb2a3f1ae2ec54b1d9e586
Anton <anthony300787@gmail.com>
2019-07-23 22:30:09 +0800

    Leetcode-149

--- leetcode/src/main/java/_149MaxPointsonaLine.java
+++ leetcode/src/main/java/_149MaxPointsonaLine.java
@@ -5,23 +5,6 @@ import java.util.*;
 
 public class _149MaxPointsonaLine {
 
-    public static void main(String... args) {
-        // 3 4 4 2 2
-        int res = 0;
-        res = new _149MaxPointsonaLine().maxPoints(new int[][]{{1, 1}, {2, 2}, {3, 3}});
-        System.out.println(res);
-        res = new _149MaxPointsonaLine().maxPoints(new int[][]{{1, 1}, {3, 2}, {5, 3}, {4, 1}, {2, 3}, {1, 4}});
-        System.out.println(res);
-        res = new _149MaxPointsonaLine().maxPoints(new int[][]{{1, 1}, {1, 1}, {2, 2}, {2, 2}});
-        System.out.println(res);
-        res = new _149MaxPointsonaLine().maxPoints(new int[][]{{1, 1}, {1, 1}});
-        System.out.println(res);
-        res = new _149MaxPointsonaLine().maxPoints(new int[][]{{0, 0}, {94911151, 94911150}, {94911152, 94911151}});
-        System.out.println(res);
-        res = new _149MaxPointsonaLine().maxPoints(new int[][]{{-1, -1}, {0, 0}, {1, 1}});
-        System.out.println(res);
-    }
-
     public int maxPoints(int[][] points) {
         if (points.length < 3) {
             return points.length;
