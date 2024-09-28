ed2ee0290498e66c1af647c030019aefe2348072
anton <anthony300787@gmail.com>
2024-09-28 18:33:12 +0800

    Leetcode-3298

---
+++ leetcode/src/main/java/_3298CountSubstringsThatCanBeRearrangedToContainStringII.java
@@ -0,0 +1,8 @@
+public class _3298CountSubstringsThatCanBeRearrangedToContainStringII {
+
+    public long validSubstringCount(String word1, String word2) {
+        //N.B.: same to 3297
+        return new _3297CountSubstringsThatCanBeRearrangedToContainStringI().validSubstringCount(word1, word2);
+    }
+
+}
