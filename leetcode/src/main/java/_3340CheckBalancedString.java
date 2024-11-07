1b04892f73884863b4a4b7ddf606c63ae428eae7
anton <anthony300787@gmail.com>
2024-11-07 15:49:12 +0800

    Leetcode-3340

---
+++ leetcode/src/main/java/_3340CheckBalancedString.java
@@ -0,0 +1,16 @@
+public class _3340CheckBalancedString {
+
+    public boolean isBalanced(String num) {
+        int odd = 0;
+        int even = 0;
+        for (int i = 0; i < num.length(); i++) {
+            if (i % 2 == 0) {
+                even += (num.charAt(i) - '0');
+            } else {
+                odd += (num.charAt(i) - '0');
+            }
+        }
+        return odd == even;
+    }
+
+}
